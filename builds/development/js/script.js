c48e1bed8218adb4851c4de2d35d718677e63c29
Dmacompton <dmacompton@gmail.com>
2015-10-26 18:07:31 +0200

    1st commit

---
+++ builds/development/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+		Detectizr.detect({detectScreen:false});
+
+		// scroll animation
+		/*
+		var waypoint = new Waypoint({
+			element: $('article .second-block .row div.girl-left'),
+			handler: function() {
+				$(this.element).animate({
+					left: '-8'
+				});
+			},
+			offset: '50%'
+		});
+	*/
+});
