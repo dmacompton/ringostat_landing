2975aa5475d5c7919df88a4f94b282887f0710f2
dmacompton <d.mihaylov@ringostat.com>
2015-12-15 08:47:57 +0200

    first day

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,17 +1,9 @@
 $(document).ready(function () {
-
-		Detectizr.detect({detectScreen:false});
-
-		// scroll animation
-		/*
-		var waypoint = new Waypoint({
-			element: $('article .second-block .row div.girl-left'),
-			handler: function() {
-				$(this.element).animate({
-					left: '-8'
-				});
-			},
-			offset: '50%'
-		});
-	*/
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 200) {
+			$('#navbar').addClass('dma');
+		} else {
+			$('#navbar').removeClass('dma');
+		}
+	});
 });
--- gulpfile.js
+++ gulpfile.js
@@ -32,9 +32,10 @@ gulp.task('css', function() {
 gulp.task('js', function() {
 	gulp.src([
 		'builds/development/js/jquery-1.10.2.min.js',
-		'builds/development/js/modernizr.js',
-		'builds/development/js/device.js',
-		'builds/development/js/jqeury.waypoints.js',
+		'builds/development/js/jquery.smooth-scroll.min.js',
+		//'builds/development/js/modernizr.js',
+		//'builds/development/js/device.js',
+		//'builds/development/js/jqeury.waypoints.js',
 		'builds/development/js/script.js'
 	])
 		.pipe(uglify())
@@ -50,8 +51,7 @@ gulp.task('html', function() {
 });
 
 gulp.task('watch', function() {
-	gulp.watch('builds/development/scss/includes/*.scss', ['css']);
-	gulp.watch('builds/development/scss/*.scss', ['css']);
+	gulp.watch('builds/development/scss/**/*.scss', ['css']);
 	gulp.watch('builds/development/index.html', ['html']);
 	gulp.watch('builds/development/js/*.js', ['js']);
 });
