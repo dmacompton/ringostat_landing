7cc93505837c5036cb093c7330a5ce6dc91d8fa7
dmacompton <d.mihaylov@ringostat.com>
2015-12-22 12:34:52 +0200

    fix

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	$(window).scroll(function(){
-		if($(window).scrollTop() > 200) {
+		if($(window).scrollTop() > 40) {
 			$('nav').addClass('navbar-dark');
 		} else {
 			$('nav').removeClass('navbar-dark');
