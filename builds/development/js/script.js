eb2344164dbadd5b83f00ea0b4c568da17c4320b
dmacompton <d.mihaylov@ringostat.com>
2016-01-22 19:12:00 +0200

    reform code

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-	$(window).scroll(function(){
-		if($(window).scrollTop() > 40) {
-			$('nav').addClass('navbar-dark');
-		} else {
-			$('nav').removeClass('navbar-dark');
-		}
-	});
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 40) {
+            $('nav').addClass('navbar-dark');
+        } else {
+            $('nav').removeClass('navbar-dark');
+        }
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,14 @@ var gulp = require('gulp'),
     htmlmin = require('gulp-htmlmin'),
     livereload = require('gulp-livereload');
 
-gulp.task('connect', function() {
+gulp.task('connect', function () {
     connect.server({
         root: 'builds/dist',
         livereload: true
     });
 });
 
-gulp.task('css', function() {
+gulp.task('css', function () {
     gulp.src('builds/development/scss/style.scss')
         .pipe(sass())
         .pipe(autoprefixer('last 15 versions'))
@@ -29,29 +29,29 @@ gulp.task('css', function() {
         .pipe(connect.reload());
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
     gulp.src([
-        'builds/development/js/jquery-1.10.2.min.js',
-        'builds/development/js/jquery.smooth-scroll.min.js',
-        'builds/development/js/bootstrap.min.js',
-        'builds/development/js/modernizr.js',
-        'builds/development/js/device.js',
-        //'builds/development/js/jqeury.waypoints.js',
-        'builds/development/js/script.js'
-    ])
+            'builds/development/js/jquery-1.10.2.min.js',
+            'builds/development/js/jquery.smooth-scroll.min.js',
+            'builds/development/js/bootstrap.min.js',
+            'builds/development/js/modernizr.js',
+            'builds/development/js/device.js',
+            //'builds/development/js/jqeury.waypoints.js',
+            'builds/development/js/script.js'
+        ])
         .pipe(uglify())
         .pipe(concat('script.min.js'))
         .pipe(gulp.dest('builds/dist/'))
 });
 
-gulp.task('html', function() {
+gulp.task('html', function () {
     gulp.src('builds/development/index.html')
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(connect.reload())
         .pipe(gulp.dest('builds/dist'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('builds/development/scss/**/*.scss', ['css']);
     gulp.watch('builds/development/index.html', ['html']);
     gulp.watch('builds/development/js/*.js', ['js']);
